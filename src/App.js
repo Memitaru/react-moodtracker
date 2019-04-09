d90ced63bbaa67cbd36a26d708af6b3fd4fd4836
Memitaru <amandatwah@gmail.com>
2019-04-08 18:00:18 -0700

    Added React router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {}
+  }
+
   render() {
     return (
       <div className="App">
